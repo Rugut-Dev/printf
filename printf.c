58b5696db49799deb3214f2d9ac11bc754ea28fd
Kelvin Rugut <krugut36@gmail.com>
2023-08-21 10:42:07 -0700

    Updates

--- printf.c
+++ printf.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _printf - custom printf
  * @format: char pointer
@@ -10,16 +9,13 @@
 int _printf(const char *format, ...)
 {
 	int char_iterator = 0;
-
 	va_list args_list;
 
 	if (format == NULL)
 	{
 		return (-1);
 	}
-
 	va_start(args_list, format);
-
 	while (*format)
 	{
 		if (*format != '%')
@@ -32,7 +28,6 @@ int _printf(const char *format, ...)
 			format++;
 			if (*format == '\0')
 				break;
-
 			if (*format == '%')
 			{
 				write(1, format, 1);
@@ -50,16 +45,11 @@ int _printf(const char *format, ...)
 				char *string = va_arg(args_list, char*);
 
 				char_iterator = strlen(string);
-
 				write(1, string, strlen(string));
 			}
-
 		}
-
 		format++;
 	}
-
 	va_end(args_list);
-
 	return (char_iterator);
 }

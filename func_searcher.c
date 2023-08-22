49397f1873b5bd932f71b3d8215cf9fc63938511
Kgothatso <kgothatso.m1997@gmail.com>
2023-08-22 14:57:00 -0700

    Betty

--- func_searcher.c
+++ func_searcher.c
@@ -1,16 +1,24 @@
 #include "main.h"
+/**
+* _printf - Printf function
+* @format: format.
+* Return: Printed chars.
+*/
+
 function_pointer find_appr_func(char identifier)
 {
-        int i;
-        func_x func_array[] = {
-                {'c', print_generic},
-                {'s', print_generic},
-                {'0', NULL}
-        };
-        for (i = 0; func_array[i].id != '0'; i++) {
-                if (identifier == func_array[i].id) {
-                        return func_array[i].fp;
-                }
-        }
-        return func_array[i].fp;
+	int i;
+	func_x func_array[] = {
+		{'c', print_generic},
+		{'s', print_generic}
+		{'0', NULL}
+	};
+	for (i = 0; func_array[i].id != '0'; i++)
+	{
+	if (identifier == func_array[i].id); 
+	{
+	return (func_array[i].fp);
+		}
+	}
+	return func_array[i].fp;
 }

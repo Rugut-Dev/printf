4fc4a90e243a8c9f34c2915fd3a33af662dc3e6f
Kelvin Rugut <krugut36@gmail.com>
2023-08-24 07:12:10 -0700

    Source code Updates

---
+++ func_searcher.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * find_appr_func - checks for teh right function
+ * @identifier: specifier
+ *
+ * Return: fp
+ */
+function_pointer find_appr_func(char identifier)
+{
+	int i;
+	func_x func_array[] = {
+		{'c', print_generic},
+		{'s', print_s},
+		{'%', print_percent},
+		{'0', NULL}
+	};
+
+	for (i = 0; func_array[i].id != '0'; i++)
+	{
+		if (identifier == func_array[i].id)
+		{
+			return (func_array[i].fp);
+		}
+	}
+	return (func_array[i].fp);
+}
